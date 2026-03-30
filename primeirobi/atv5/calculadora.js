86adf2e214b6bc963bb4708f37c53e9bc7f6bd26
pedrojoaofreitasdasilvafacul-cloud <pedrojoaofreitasdasilvafacul@gmail.com>
2026-03-30 01:36:43 -0300

    atv5 feita

---
+++ primeirobi/atv5/calculadora.js
@@ -0,0 +1,12 @@
+
+const valorConta = 100;
+
+const percentualGorjeta = 15;
+
+const valorgorjeta = (valorConta * percentualGorjeta)/100;
+
+const valortotal = valorConta + valorgorjeta ;
+
+console.log(`Valor da conta: R$${valorConta.toFixed(2)}, Gorjeta (${percentualGorjeta}%): R$${valorgorjeta.toFixed(2)}, Total a Pagar: R$${valortotal.toFixed(2)}`)
+
+
